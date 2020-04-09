0bafd2df939a611d0c1736fc2a79b3a73acf0e4a
Cal <58495896+cal-frye@users.noreply.github.com>
2020-04-08 23:29:50 -0400

    Updated bless-related commands

--- index.js
+++ index.js
@@ -7,10 +7,24 @@ client.once('ready', () => {
 })
 
 client.on('message', message => {
-    if (message.content.startsWith(`${prefix}bless`)) {
-        message.channel.send("Bless you, my child. May the good lord Pepe rain many dank memes upon your feed this day, now and forever. Amen.", {
-            files: ["https://media.giphy.com/media/Ko6eiEhQRYQVO/giphy.gif"]
-        })
+    
+    let args = message.content.substring(prefix.length).split(" ");
+    
+    switch(args[0]) {
+        case 'bless' :
+
+            if(args[1] === 'chat') {
+                message.channel.send('OH LORD! Bless this chat in the name of the Papa :pizza:, the Pepe :frog:, and the Pu$$y :ok_hand:. Amen.');
+            } else if(!args[1]) {
+                message.channel.send('Bless you, my child. May the good lord Pepe :frog: rain many dank memes upon your feed this day, now and forever. Amen. :pray:', {
+                    files: ["https://media.giphy.com/media/Ko6eiEhQRYQVO/giphy.gif"]
+                    });
+            } else {
+                message.channel.send('While I do not understand your request, my child, I nonetheless bless you, this chat, and all who meme within it.', {
+                    files: ["https://media.giphy.com/media/ZSuL021jBST2E/giphy.gif"]
+                });
+            }
+        break;
     }
 })
 
